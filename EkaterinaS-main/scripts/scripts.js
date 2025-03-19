8bbc992aa829910a55bdb0ac998dc79406762c1d
Ekaterina-Sofronova <vfvfrfnbb2004@gmail.com>
2025-03-19 16:56:59 +0300

    Add files via upload

--- EkaterinaS-main/scripts/scripts.js
+++ EkaterinaS-main/scripts/scripts.js
@@ -1,43 +1,78 @@
 'use strict'
-
 document.addEventListener("DOMContentLoaded", () => {
     console.log('Скрипт отработал корректно')
 
     /* 1. Динамическое перелистывание карточек в блоке "specialist__list" */
-    const slider__left = document.querySelector('slider__left');    // создаем переменную находя блок по классу
-    const slider__right = document.querySelector('slider__right');
-    const cards = document/getEllementById('specialist__list')
-        /* 
-        *   Алгоритм
-        *
-        *   1. Начало
-        *   2. Имеется 2 кновки: влево и вправо
-        *   3. Нажимаем на кнопки и ожидаем листание карточек
-        *       3.1. Нажимаем на кнопку вправо
-        *           3.1.1 Происходит листание карточек справа налево поочередно одна за другой    
-        *       3.2. Нажимаем на кнопку влево 
-        *           3.2.1. Происходит листание карточек слева направо поочерено
-        *   4. Конец
-        * 
-        *   Блок-схема: /images/block-schema.png
-        */
-        If (slider__left) {
-            console.log('Константа slider __left существует');
-            slider__left.addEventListener("click", () -> {
+    const sliderLeft = document.querySelector('.sliderLeft');    // создаем переменную находя блок по классу
+    const sliderRight = document.querySelector('.sliderRight');
+    const cards = document.querySelector('.specialist__list')
+    /* 
+    *   Алгоритм
+    *
+    *   1. Начало
+    *   2. Имеется 2 кнопки: влево и вправо
+    *   3. Нажимаем на кнопки и ожидаем листание карточек
+    *       3.1. Нажимаем на кнопку вправо
+    *           3.1.1 Происходит листание карточек справа налево поочередно одна за другой    
+    *       3.2. Нажимаем на кнопку влево 
+    *           3.2.1. Происходит листание карточек слева направо поочередно
+    *   4. Конец
+    * 
+    *   Блок-схема: /images/block-schema.png
+    */
+    if (sliderLeft) {
+        console.log('Константа sliderLeft существует');
+        sliderLeft.addEventListener("click", () => {
             console.log("Кнопка нажимается");
-            let left - cards. style.getPropertyValue('left');
+            let left = cards.style.getPropertyValue('left');
             if (left) {
-                left - left.replace('px', '');
-                left - parseint(left);
+                left = left.replace('px', '');
+                left = parseInt(left);
             }
             else {
                 left = 0;
             }
-            if (left<720)
-            {
+            if (left < 1120) {
                 left = 0;
             } else {
-                left=left + 720;
+                left = left + 1120;
             }
             console.log('left ' + left);
-})
+            cards.style.setProperty('left', left + 'px');
+        })
+    }
+    if (sliderRight) {
+        console.log('Константа sliderRight существует');
+        sliderRight.addEventListener('click', () => {
+            console.log('Кнопка нажимается');
+            let width = 360 * 4;
+            console.log('width' + width);
+            /*let left = cards.style.getPropertryValue('left');
+            if (left) {
+                left = left.replace('px', '');
+        }
+                else {
+                    left = 0;
+                }
+            left = perseInt(left) - 350;
+            console.log('left ' + left);
+            cards.stule.setProperty('left', left + 'px');*/
+            let left = cards.style.getPropertyValue('left');
+            if (left) {
+                left = left.replace('px', '');
+                left = parseInt(left);
+            }
+            else {
+                left = 0;
+            }
+            if (left >= width - 1120) {
+                left = width - 1120;
+            } else {
+                left = left - 1120;
+            }
+            console.log('left ' + left);
+            cards.style.setProperty('left', left + 'px');
+        })
+    }
+    console.log('Скрипт отработал корректно')
+});
